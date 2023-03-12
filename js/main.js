b0356313e506d715a6f192271e6b67a6c63be039
Deba2k03 <110084866+Deba2k03@users.noreply.github.com>
2023-03-12 19:15:24 +0530

    s3

---
+++ js/main.js
@@ -0,0 +1,21 @@
+`use strict`;
+function refreshTime() {
+  const timeDisplay = document.getElementById("time");
+  const dateString = new Date().toLocaleString();
+  const formattedString = dateString.replace(", ", " - ");
+  timeDisplay.textContent = formattedString;
+}
+  setInterval(refreshTime, 1000);
+  `use strict`
+  var datetime = new Date().getDay();
+  console.log(datetime); // it will represent date in the console of developers tool
+  document.getElementById("time").textContent = datetime; //it will print on html page
+
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
